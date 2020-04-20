37ad2ff157011f520a9e1a82d73f626bb2b6f760
Lijiaxuan <17722009@bjtu.edu.cn>
2020-04-20 17:34:08 +0800

    second

--- src/main/java/com/overwatch/covid/Database.java
+++ src/main/java/com/overwatch/covid/Database.java
@@ -3,11 +3,17 @@ package com.overwatch.covid;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.bson.Document;
+import org.bson.BsonArray;
+import org.bson.BsonDocument;
+import org.bson.BsonValue;
 
+import com.covid.DataBase.UpdateDatabase;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -19,7 +25,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.Filters;
-
+import org.jsoup.Jsoup;
 
 /*
  *  all methods in this class should be declared in static
@@ -30,10 +36,12 @@ import com.mongodb.client.model.Filters;
 public final class Database implements UpdateInterface, RequestInterface
 {
 	
-	private static MongoCollection<Document> covid;
+	private static MongoCollection<BsonDocument> covid;
 	private static Database myself;
 	private String data;
-	 MongoDatabase countrydb;
+	static MongoDatabase countrydb;
+	static MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+	
 	private Database()
 	{
 		// TODO
@@ -52,27 +60,35 @@ public final class Database implements UpdateInterface, RequestInterface
 	}
 
 
-	public void connection() {
+	public static void connection() {
 		  
-		  MongoClient mongoClient = new MongoClient( "localhost" , 27017 );
+		
 		    
+		    // 连接到数据库
 		 countrydb = mongoClient.getDatabase("countrydb");  
 		  System.out.println("Connect to database successfully");
 		  
-	    covid = countrydb.getCollection("covid");
+	    covid = countrydb.getCollection("covid",BsonDocument.class);
 	       System.out.println("collection covid accesses succeed ");
 			
 	}
 	
 	
-	public String getCollection() {
+	public String getCollection() {//collection中所有数据
 	     	
 		 connection();
 		 String all = null;
 	       try{   
 		    
-		         FindIterable<Document> findIterable = covid.find();  
-		         MongoCursor<Document> mongoCursor = findIterable.iterator();  
+		         //检索所有文档  
+		         /** 
+		         * 1. 获取迭代器FindIterable<Document> 
+		         * 2. 获取游标MongoCursor<Document> 
+		         * 3. 通过游标遍历检索出的文档集合 
+		         * */  
+		     
+		         FindIterable<BsonDocument> findIterable = covid.find();  
+		         MongoCursor<BsonDocument> mongoCursor = findIterable.iterator();  
 		    while(mongoCursor.hasNext()){  
 	            all = mongoCursor.next().toString();
 	            System.out.println(all);  
@@ -86,10 +102,10 @@ public final class Database implements UpdateInterface, RequestInterface
 	
 	
     public String getChinaData() {
-    	
+    	 //获取集合中 中国的数据
 		   connection();
-		    FindIterable<Document> findIterable = covid.find(Filters.eq("name", "China"));  
-	        MongoCursor<Document> mongoCursor = findIterable.iterator();  
+		    FindIterable<BsonDocument> findIterable = covid.find(Filters.eq("name", "China"));  
+	        MongoCursor<BsonDocument> mongoCursor = findIterable.iterator();  
 	     	String China = mongoCursor.next().toString();
 			System.out.print(China);
 			return China;
@@ -101,7 +117,10 @@ public final class Database implements UpdateInterface, RequestInterface
 	public String getData()
 	{
 		// TODO
-		return null;
+		
+		
+		
+		return data;
 	}
 
 	
@@ -109,31 +128,56 @@ public final class Database implements UpdateInterface, RequestInterface
 	public void sendData(String data)
 	{
 		// TODO
+		this.data = data;
 	}
 
 	
-	
-	
-	
-	
-	
-	
-	public void  storeData() {
-		sendData(data);
-		getData();
-		//先将string转为json 再储存
-	   
+	public void  storeData(String data) {
 		
-		
-		List<Document> documentList = new ArrayList<>();
-		for(Data data : dataList) {
-			Document document = new Document("_id",1)
-					.append("countey", data.getCountry())
-					.append("newcases",data.getNewcases())
+		//先将json的string转 document 再储存
+          
+		String json = " {" + "'_id':'1'"+
+                    " 'name' : 'england', " +
+                    " 'newcases' : '10', " +
+                    " 'death' : '100', " +
+                    " 'total' : '200' " +
+                    " } ";
+       BsonDocument document = BsonDocument.parse(json);
+       covid.insertOne(document );
+               
+     
+                BsonArray bsonArray;
+                bsonArray =  BsonArray.parse(data);//把json格式的string导入这个数组
+                 List<BsonDocument> documents = new LinkedList<>();
+              //  String info;
+                BsonDocument bson;
+                for(BsonValue bsonValue : bsonArray){//put bson into document 
+                     bson = bsonValue.asDocument();
+                     documents.add(bson);
+               /*     info = String.format("%s :%s\t%s\t%s\t", bson.getString("countryname").getValue(), 
+                        bson.getString("newcases").getValue(), bson.getString("death").getValue(),
+                        bson.getString("total").getValue());
+                    System.out.println(info);
+                    */
+                  
+                 
+                    
+                    
+                }
+                exportMongo(documents);
 		}
+	
+	
+	
+	
 		
-	}
-	
-	
+	 private static void exportMongo( List<BsonDocument>documents){       
+	    
+	        connection();
+	       
+	        covid.insertMany(documents);
+	        System.out.println("\n数据导出MongoDB成功!");
+	        mongoClient.close();
+	 }
 	
 }
