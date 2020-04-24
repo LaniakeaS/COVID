bac07368cacf124d2d0565a1e83e3e5ffdce67aa
Hanzhong_Qi <731619643@qq.com>
2020-04-24 15:37:16 +0800

    Hanzhong-12: First Test

--- src/main/java/com/overwatch/covid/Server.java
+++ src/main/java/com/overwatch/covid/Server.java
@@ -8,16 +8,16 @@ public final class Server
 {
 	private static Database database;
 	private static UpdateDatabase update;
-	private static HandleRequest handle;
+	//private static HandleRequest handle;
 	
 	private Server()
 	{
 		Database.createDabatase();
 		database = Database.getDatabase();
 		update = new UpdateDatabase(database);
-		handle = new HandleRequest(database);
+		//handle = new HandleRequest(database);
 		update.start();
-		handle.start();
+		//handle.start();
 	}
 	
 	public static void startServer()
